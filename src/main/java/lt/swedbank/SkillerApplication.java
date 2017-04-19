437acb490e6f1156654c506ae17a512a61349e40
PaulKutka <kutka100@gmail.com>
2017-04-19 22:42:33 +0300

    Initial commit

---
+++ src/main/java/lt/swedbank/SkillerApplication.java
@@ -0,0 +1,12 @@
+package lt.swedbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillerApplication.class, args);
+	}
+}

5953a747db364fc9bce3a108a1289b4a47fdd341
DanikRok <uzastish@gmail.com>
2017-05-11 14:28:26 +0300

    added unit test for /user/get with skills

--- src/test/java/lt/swedbank/controllers/user/UserControllerTest.java
+++ src/test/java/lt/swedbank/controllers/user/UserControllerTest.java
@@ -1,7 +1,9 @@
 package lt.swedbank.controllers.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lt.swedbank.beans.entity.Skill;
 import lt.swedbank.beans.entity.User;
+import lt.swedbank.services.user.IUserService;
 import lt.swedbank.services.user.UserService;
 import org.junit.After;
 import org.junit.Before;
@@ -15,13 +17,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Created by paulius on 5/4/17.
- */
+
 public class UserControllerTest {
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
@@ -36,6 +43,7 @@ public class UserControllerTest {
 
 
     private User correctUser;
+    private List<Skill> correctSkills;
 
     @InjectMocks
     private UserController userController;
@@ -47,15 +55,25 @@ public class UserControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        mockMvc = MockMvcBuilders.standaloneSetup(userController)./*addFilters(new CorsFilter()).*/build();
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
 
         mapper = new ObjectMapper();
 
+        Long userId = Integer.toUnsignedLong(0);
+
         correctUser = new User();
+        correctUser.setId(userId);
         correctUser.setName("TestUserName");
         correctUser.setLastName("TestUserLastName");
         correctUser.setPassword("TestUserPassword");
         correctUser.setEmail("testuser@gmail.com");
+
+        correctSkills = new ArrayList<>();
+        correctSkills.add(new Skill("SkillName1", userId));
+        correctSkills.add(new Skill("SkillName2", userId));
+        correctSkills.add(new Skill("SkillName3", userId));
+
+        correctUser.setSkills(correctSkills);
     }
 
     @After
@@ -63,19 +81,21 @@ public class UserControllerTest {
     }
 
     @Test
-    public void getUser() throws Exception {
+    public void get_user_success() throws Exception {
+
+        when(userService.getUserByEmail(any())).thenReturn(correctUser);
+        mockMvc.perform(get("/user/get").header("Authorization", "Bearer").requestAttr("email", "a@a.a"))
+                .andExpect(status().isOk())
+                //.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))//Irrelevant while skills array is hardcoded
+                .andExpect(jsonPath("$.name", is("TestUserName")))
+                .andExpect(jsonPath("$.lastName", is("TestUserLastName")))
+                .andExpect(jsonPath("$.email", is("testuser@gmail.com")))
+                .andExpect(jsonPath("$.skills", hasSize(3)))
+                .andExpect(jsonPath("$.skills[0].title", is("SkillName1")))
+                .andExpect(jsonPath("$.skills[1].title", is("SkillName2")))
+                .andExpect(jsonPath("$.skills[2].title", is("SkillName3")));
+        verify(userService, times(1)).getUserByEmail(any());
+        verifyNoMoreInteractions(userService);
     }
-
-
-    @Test
-    public void test_if_unauthorized_when_token_not_provided() throws Exception {
-
-        //TODO Write a test for unauthorized access. Need to provide good headers.
-        mockMvc.perform(get("/user/get")
-                .contentType(contentType))
-                .andExpect(status().isBadRequest());
-    }
-
-
 
 }

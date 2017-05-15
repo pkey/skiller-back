7470ed7719bde77a5364cb4548cd28ab1c31b781
vilius571 <vilius571@gmail.com>
2017-05-15 15:12:15 +0300

    Update RestResponseEntityExceptionHandler.java

--- src/main/java/lt/swedbank/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/lt/swedbank/handlers/RestResponseEntityExceptionHandler.java
@@ -5,7 +5,7 @@ import com.auth0.exception.Auth0Exception;
 import lt.swedbank.beans.response.AuthenticationError;
 import lt.swedbank.beans.response.AuthenticationErrorsWrapper;
 import lt.swedbank.beans.response.ErrorResponse;
-import lt.swedbank.exceptions.skill.SkillAlreadyAddedToUserException;
+import lt.swedbank.exceptions.skill.SkillAlreadyExistsException;
 import lt.swedbank.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;

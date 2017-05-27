74834b23b0301049bb7a695ce8047a7c7b7d5da4
PaulKutka <kutka100@gmail.com>
2017-05-27 17:51:59 +0300

    Search by name

--- src/main/java/lt/swedbank/repositories/UserSearch.java
+++ src/main/java/lt/swedbank/repositories/UserSearch.java
@@ -2,7 +2,9 @@ package lt.swedbank.repositories;
 
 
 import lt.swedbank.beans.entity.User;
+import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.query.dsl.BooleanJunction;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.stereotype.Repository;
 
@@ -31,16 +33,19 @@ public class UserSearch {
                         // .overridesForField( "name", "customanalyzer_query" )
                         .get();
 
-        String queryText = "*" + text.toLowerCase() + "*";
+        String queryText = "petras juozas";
+
+        String[] qeueries = {"*etra*", "*uoza*"};
 
         // a very basic query by keywords
-        org.apache.lucene.search.Query query =
-                queryBuilder
-                        .keyword()
-                        .wildcard()
-                        .onFields("name", "lastName")
-                        .matching(queryText)
-                        .createQuery();
+        BooleanJunction bool =
+                queryBuilder.bool();
+
+        for (String element: qeueries) {
+            bool.must(queryBuilder.keyword().wildcard().onFields("name", "lastName").matching(element).createQuery());
+        }
+
+        Query query = bool.createQuery();
 
         // wrap Lucene query in an Hibernate Query object
         org.hibernate.search.jpa.FullTextQuery jpaQuery =

8563a57a2e1898bd07f7178ec8c44e63489e6ff1
vilius <vilius571@gmail.com>
2017-06-20 15:33:30 +0300

    approvers and disaprovers repos fixed:

---
+++ src/main/java/lt/swedbank/repositories/DisaproversRepository.java
@@ -0,0 +1,8 @@
+package lt.swedbank.repositories;
+
+
+import lt.swedbank.beans.entity.Disapprover;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DisaproversRepository extends CrudRepository<Disapprover, Long>{
+}

b1de330ad8c762c2a347a35e48b4c1587ece2c07
Mantas Boronilscikovas <p998bob@fspa.myntet.se>
2017-07-26 17:04:29 +0300

    delted unused method

--- src/main/java/lt/swedbank/services/user/UserService.java
+++ src/main/java/lt/swedbank/services/user/UserService.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
